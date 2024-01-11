0679b39caca523273c4d602eba039500e7371266
Joaquin Guerrero <joaquinsgro@gmail.com>
2024-01-10 22:43:58 -0600

    Modified Projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -13,28 +13,28 @@ export function Projects({id}){
         <section className={style.son2}>
             <div className={style.sonp}>
                 <p>Proyecto dedicado a los servicios de cualquier tipo...</p>
-                <a href="https://workify-alpha.vercel.app/home">Visitar</a>
+                <a href="https://workify-alpha.vercel.app/home" target="_blank" rel="noopener noreferrer">Visitar</a>
             </div>
         </section>
 
         <section className={style.son3}>
             <div className={style.sonp}>
                 <p>Juego de memoria</p>
-                <a href="https://memory-game-psi-murex.vercel.app/">Visitar</a>
+                <a href="https://memory-game-psi-murex.vercel.app/" target="_blank" rel="noopener noreferrer">Visitar</a>
             </div>
         </section>
 
         <section className={style.son4}>
             <div className={style.sonp}>
                 <p>Pokédex</p>
-                <a href="https://pokedex-eight-rho.vercel.app/">Visitar</a>
+                <a href="https://pokedex-eight-rho.vercel.app" target="_blank" rel="noopener noreferrer">Visitar</a>
             </div>
         </section>
 
          <section className={style.son1}>
             <div className={style.sonp}>
                 <p>Grosgar una aplicación de servicios contables</p>
-                <a href="https://grosgar.vercel.app/">Visitar</a>
+                <a href="https://grosgar.vercel.app/" target="_blank" rel="noopener noreferrer">Visitar</a>
             </div>
         </section>
 
