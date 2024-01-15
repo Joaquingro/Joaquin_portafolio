33c8d222c7642299a7596fa908e9851ca61a9e71
Joaquin Guerrero <joaquinsgro@gmail.com>
2024-01-14 23:09:24 -0600

    Responsive Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -105,7 +105,7 @@ export function Contact({id}) {
             fullWidth
             style={{
                 backgroundColor: "white",
-                borderRadius: "5px"
+                borderRadius: "5px",
 
             }}
             error={!!errors.name}
