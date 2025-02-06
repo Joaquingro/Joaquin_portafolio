08f94b4e94717f6e937f3829036db7e800e63df2
Joaquin Guerrero <joaquinsgro@gmail.com>
2025-02-06 10:38:07 -0600

    Nuevo proyecto agregado

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,45 +1,59 @@
 import style from "../Projects/Projects.module.css";
-import 'animate.css';
+import "animate.css";
 
-export function Projects({id}){
-    return (
-        <main id = {id} className={style.container}>
-           <header className={style.title}>
-                <h2>MIS PROYECTOS</h2>
-                <h1>Mis proyectos</h1>
-            </header>
-            
-        <aside className={style.containerSon}>
-        <article className={style.son2}>
+export function Projects({ id }) {
+  const projects = [
+    {
+      id: 1,
+      title: "Proyecto dedicado a los servicios de cualquier tipo...",
+      link: "https://workify-alpha.vercel.app/home",
+      styleClass: style.son2,
+    },
+    {
+      id: 2,
+      title: "Juego de memoria",
+      link: "https://memory-game-psi-murex.vercel.app/",
+      styleClass: style.son3,
+    },
+    {
+      id: 3,
+      title: "Pokédex",
+      link: "https://pokedex-eight-rho.vercel.app",
+      styleClass: style.son4,
+    },
+    {
+      id: 4,
+      title: "Grosgar una aplicación de servicios contables",
+      link: "https://grosgar.vercel.app/",
+      styleClass: style.son1,
+    },
+    {
+        id: 5,
+        title: "Binkio",
+        link: "https://app.binkio.io/",
+        styleClass: style.son5,
+      },
+  ];
+
+  return (
+    <main id={id} className={style.container}>
+      <header className={style.title}>
+        <h2>MIS PROYECTOS</h2>
+        <h1>Mis proyectos</h1>
+      </header>
+
+      <aside className={style.containerSon}>
+        {projects.map((project) => (
+          <article key={project.id} className={project.styleClass}>
             <div className={style.sonp}>
-                <p>Proyecto dedicado a los servicios de cualquier tipo...</p>
-                <a href="https://workify-alpha.vercel.app/home" target="_blank" rel="noopener noreferrer">Visitar</a>
+              <p>{project.title}</p>
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
+                Visitar
+              </a>
             </div>
-        </article>
-
-        <article className={style.son3}>
-            <div className={style.sonp}>
-                <p>Juego de memoria</p>
-                <a href="https://memory-game-psi-murex.vercel.app/" target="_blank" rel="noopener noreferrer">Visitar</a>
-            </div>
-        </article>
-
-        <article className={style.son4}>
-            <div className={style.sonp}>
-                <p>Pokédex</p>
-                <a href="https://pokedex-eight-rho.vercel.app" target="_blank" rel="noopener noreferrer">Visitar</a>
-            </div>
-        </article>
-
-         <article className={style.son1}>
-            <div className={style.sonp}>
-                <p>Grosgar una aplicación de servicios contables</p>
-                <a href="https://grosgar.vercel.app/" target="_blank" rel="noopener noreferrer">Visitar</a>
-            </div>
-        </article>
-
-        </aside>
-        </main>
-        
-    );
+          </article>
+        ))}
+      </aside>
+    </main>
+  );
 }
--- src/components/School/School.jsx
+++ src/components/School/School.jsx
@@ -1,67 +1,79 @@
 import style from "../School/School.module.css";
-import joaquin from "../../assets/joaquin.png"
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import Button from '@mui/material/Button';
-
+import joaquin from "../../assets/joaquin.png";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import Button from "@mui/material/Button";
 
 export function School(props) {
-    const {id} = props;
+  const { id } = props;
 
-    const cvUrl = 'https://drive.google.com/file/d/11z4YRAoEufwEXxHSmlfhkGKtWU4hjJrf/view?usp=sharing'; 
+  const cvUrl =
+    "https://drive.google.com/file/d/11z4YRAoEufwEXxHSmlfhkGKtWU4hjJrf/view?usp=sharing";
 
-    const handleDownload = () => {
-      
-      window.open(cvUrl, '_blank');
-    };
+  const handleDownload = () => {
+    window.open(cvUrl, "_blank");
+  };
 
-    return (
-    <main id = {id} className={style.container}>
-        <section className={style.son}>
-            <header className={style.title}>
-                    <h2>ACERCA DE MI</h2>
-                    <h1>Acerca de mi</h1>
-            </header>
-                <article className={style.baby}>
-                    <div className={style.imgProfile}>
-                        <img src={joaquin} alt="Imagen de Joaquin" />
-                    </div>
+  return (
+    <main id={id} className={style.container}>
+      <section className={style.son}>
+        <header className={style.title}>
+          <h2>ACERCA DE MI</h2>
+          <h1>Acerca de mi</h1>
+        </header>
+        <article className={style.baby}>
+          <div className={style.imgProfile}>
+            <img src={joaquin} alt="Imagen de Joaquin" />
+          </div>
 
-                <div className={style.info}>
-                    <h3>¡Hola, soy Joaquin Guerrero!</h3>
-                    <h4>Desarrollador Web Full Stack</h4>
-                    <p> Soy un apasionado desarrollador Web Full Stack e Ingeniero en Sistemas con un enfoque creativo y soluciones innovadoras.
-                    Con una sólida formación en Ingeniería de Sistemas y una pasión innata por la programación, he dedicado mi carrera a convertir ideas en experiencias digitales impactantes. </p>
+          <div className={style.info}>
+            <h3>¡Hola, soy Joaquin Guerrero!</h3>
+            <h4>Desarrollador Web Full Stack</h4>
+            <p>
+              {" "}
+              Soy un apasionado desarrollador Web Full Stack e Ingeniero en
+              Sistemas con un enfoque creativo y soluciones innovadoras. Con una
+              sólida formación en Ingeniería de Sistemas y una pasión innata por
+              la programación, he dedicado mi carrera a convertir ideas en
+              experiencias digitales impactantes.{" "}
+            </p>
 
-                    <span className={style.target}>
-                        <p>Cumpleaños </p> 
-                        <ArrowForwardIosIcon className={style.arrow}/>
-                        <p>03-Abril-1996</p> 
-                    </span>
-                    <span className={style.target}>
-                        <p>Teléfono</p> 
-                        <ArrowForwardIosIcon className={style.arrow}/>
-                        <p>+52 5549511434</p>
-                    </span>
-                    <span className={style.target}>
-                        <p>Email</p> 
-                        <ArrowForwardIosIcon className={style.arrow}/>
-                        <p>joaquinsgro@gmail.com</p>
-                    </span>
-                    <span className={style.target}>
-                        <p>Idiomas</p> 
-                        <ArrowForwardIosIcon className={style.arrow}/>
-                        <p>Español (Nativo) - Inglés (B1) </p>
-                    </span>
+            <span className={style.target}>
+              <p>Cumpleaños </p>
+              <ArrowForwardIosIcon className={style.arrow} />
+              <p>03-Abril-1996</p>
+            </span>
+            <span className={style.target}>
+              <p>Teléfono</p>
+              <ArrowForwardIosIcon className={style.arrow} />
+              <p>+52 5549511434</p>
+            </span>
+            <span className={style.target}>
+              <p>Email</p>
+              <ArrowForwardIosIcon className={style.arrow} />
+              <p>joaquinsgro@gmail.com</p>
+            </span>
+            <span className={style.target}>
+              <p>Idiomas</p>
+              <ArrowForwardIosIcon className={style.arrow} />
+              <p>Español (Nativo) - Inglés (B1) </p>
+            </span>
 
-                    <Button 
-                    sx={{ borderColor: 'orange', color: 'orange', width: "max-content", borderRadius: "15px", marginTop: "1em" }} 
-                    variant="outlined"
-                    onClick={handleDownload}
-                    >Descargar CV</Button>
-
-                </div>
-            </article>
-        </section>
+            <Button
+              sx={{
+                borderColor: "orange",
+                color: "orange",
+                width: "max-content",
+                borderRadius: "15px",
+                marginTop: "1em",
+              }}
+              variant="outlined"
+              onClick={handleDownload}
+            >
+              Descargar CV
+            </Button>
+          </div>
+        </article>
+      </section>
     </main>
-    );
+  );
 }
